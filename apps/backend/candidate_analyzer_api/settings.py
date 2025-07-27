83eedd3a3922ed09ae16a2a222245004a028522c
Fausto Fedele <faustofedele2013@gmail.com>
2025-07-27 15:51:37 -0300

    feat: docker & PostgreSQL setup

--- apps/backend/candidate_analyzer_api/settings.py
+++ apps/backend/candidate_analyzer_api/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+from decouple import config
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Load from .env
+SECRET_KEY = os.getenv("OPENAI_API_KEY", "fallback-secret-key")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,12 +13,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rcsspknmp0qpdbc51%*6afh=#q)@^d=ucr@k(paq_oc5-emrco"
+SECRET_KEY = config("DJANGO_SECRET_KEY", default="insecure-default")
+
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -70,8 +77,12 @@ WSGI_APPLICATION = "candidate_analyzer_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB", default="ambition_candidate_analyzer"),
+        "USER": config("POSTGRES_USER", default="root"),
+        "PASSWORD": config("POSTGRES_PASSWORD", default="root"),
+        "HOST": "db",  # matches docker-compose service name
+        "PORT": "5432",
     }
 }
 
@@ -110,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- apps/backend/candidates/views.py
+++ apps/backend/candidates/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Load from .env
+load_dotenv()
 SECRET_KEY = os.getenv("OPENAI_API_KEY", "fallback-secret-key")
 
 
